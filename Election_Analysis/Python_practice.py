63dbdc56ca1ff8f845f5405a1d8685924525ca26
ckkhalaf <113135505+ckkhalaf@users.noreply.github.com>
2022-09-29 12:49:36 -0500

    Add files via upload

---
+++ Election_Analysis/Python_practice.py
@@ -0,0 +1,13 @@
+counties = ["Arapahoe","Denver","Jefferson"]
+if "El Paso" in counties:
+    print("El Paso is in the list of counties.")
+else:
+    print("El Paso is not the list of counties.")
+
+for county in counties:
+    print(county)
+    counties_dict = {"Arapahoe": 369237, "Denver":413229, "Jefferson": 390222}
+for county, voters in counties_dict.items():
+    print(f"{county} county has {voters} registered voters.")
+for county, voters in counties_dict.items():
+    print(f"{county} county has {voters} registered voters.")
